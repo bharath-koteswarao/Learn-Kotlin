9b1e2d3c244c9d0db44e3636cab81c8266ba564d
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-07-16 19:38:37 +0530

    Hello World

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,4 +1,17 @@
 /**
- * Created by koteswarao on 16-07-2017.
- * ${CLASS}
- */
+* Created by koteswarao on 16-07-2017.
+*/
+
+fun main(args:Array<String>) {
+    // Val is immutable Declaring val is same as declaring final in java
+
+    val a=10
+    val b:Int    // Type is required when not initialized
+
+    val c=20 // Int is automatically taken
+
+    println("Hello world")
+
+    println("$a $c") // Equivalent to System.out.println(a+" "+c) in java
+
+}
