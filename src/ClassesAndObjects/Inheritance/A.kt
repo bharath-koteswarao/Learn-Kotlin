531e9c68fc163d9034667d6e97822c17acec468d
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-12-13 22:04:29 +0530

    Basic Constructor

---
+++ src/ClassesAndObjects/Inheritance/A.kt
@@ -0,0 +1,12 @@
+package ClassesAndObjects.Inheritance
+
+
+/*
+ * Created by bk on 13-12-2017 21:55
+ */
+
+open class First {
+    init {
+        println("Constructor from first class")
+    }
+}
---
+++ src/ClassesAndObjects/Inheritance/B.kt
@@ -0,0 +1,12 @@
+package ClassesAndObjects.Inheritance
+
+
+/*
+ * Created by bk on 13-12-2017 21:55
+ */
+
+class Second : First() {
+    init {
+        println("This class inherits the first class")
+    }
+}
